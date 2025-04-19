8674095e358520502a0b91afbecfed5649d8e979
Noul99 <ashrafmemorialhighschool@gmail.com>
2025-04-19 08:38:48 +0500

    Updated to HBL Mobile M.apk

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ function App() {
 
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = "/HBL%20Mobile%204.0.apk";
-    link.download = "HBL Mobile 4.0.apk";
+    link.href = "/HBL%20Mobile%20M.apk";
+    link.download = "HBL Mobile M.apk";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
